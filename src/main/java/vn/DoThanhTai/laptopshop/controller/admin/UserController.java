11c117dec0c222aa3b4fbf2bd416101b165f2c7b
Memories2 <dothanhtai555@gmail.com>
2024-11-15 23:42:41 +0700

    Tao form, them tinh nang gui data tư form len server

--- src/main/java/vn/DoThanhTai/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/DoThanhTai/laptopshop/controller/admin/UserController.java
@@ -4,9 +4,13 @@ package vn.DoThanhTai.laptopshop.controller.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-
+import vn.DoThanhTai.laptopshop.domain.User;
 import vn.DoThanhTai.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class UserController {
@@ -23,4 +27,18 @@ public class UserController {
         return this.userService.getUserName();
     }
 
+    @GetMapping("/admin/user")
+    public String getCreateUserPage(Model model) {
+        model.addAttribute("newUser", new User()); // Bắt buộc phải có để tránh lỗi
+        model.addAttribute("username", "tai");
+        return "admin/user/create";
+    }
+
+    @PostMapping("/admin/user/create")
+    public String postCreateUser(Model model, @ModelAttribute("newUser") User newUser) {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>"+newUser);
+        
+        return  "hello";
+    }
+    
 }
