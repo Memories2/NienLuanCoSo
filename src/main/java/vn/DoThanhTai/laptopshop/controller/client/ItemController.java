919c465a2338b0e1d12be25bed1de85b269951b7
Memories2 <dothanhtai555@gmail.com>
2024-11-22 22:26:22 +0700

    Hoan thien xem chi tiet san pham tai client

--- src/main/java/vn/DoThanhTai/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/DoThanhTai/laptopshop/controller/client/ItemController.java
@@ -6,11 +6,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.DoThanhTai.laptopshop.domain.Product;
+import vn.DoThanhTai.laptopshop.service.ProductService;
+
 @Controller
 public class ItemController {
 
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/detail/{id}")
     public String getProductDetailPage(Model model, @PathVariable Long id) {
+        Product product = this.productService.getProductById(id);
+        model.addAttribute("product", product);
         return "client/product/detail";
     }
 }
