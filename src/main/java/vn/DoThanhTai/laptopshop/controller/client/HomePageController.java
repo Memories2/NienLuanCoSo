ea9bce9b1b4035d70efa49c7945b8d069abc119c
Memories2 <dothanhtai555@gmail.com>
2024-11-23 00:04:55 +0700

    register p1

--- src/main/java/vn/DoThanhTai/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/DoThanhTai/laptopshop/controller/client/HomePageController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import vn.DoThanhTai.laptopshop.domain.Product;
+import vn.DoThanhTai.laptopshop.domain.dto.RegisterDTO;
 import vn.DoThanhTai.laptopshop.service.ProductService;
 
 
@@ -26,5 +29,16 @@ public class HomePageController {
         model.addAttribute("products", products);
         return "client/homepage/show";
     }
-    
+
+    //////////////////////////////// Register ////////////////////////////////
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
+        return "client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("registerUser") RegisterDTO registerUser) {
+        return "client/auth/register";
+    }
 }
